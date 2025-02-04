a856c4f1b93affc1cfe871a10cb20c6df1922350
garryrk <rokhum18sep@gmail.com>
2025-02-04 09:55:23 +0530

    Create 1800. Maximum Ascending Subarray Sum.cpp

---
+++ python/1800. Maximum Ascending Subarray Sum.cpp
@@ -0,0 +1,12 @@
+class Solution:
+    def maxAscendingSum(self, nums: List[int]) -> int:
+        summ=nums[0]
+        ans=nums[0]
+        n=len(nums)
+        for i in range(1,n):
+            if(nums[i]>nums[i-1]):
+                summ+=nums[i]
+            else:
+                summ=nums[i]
+            ans=max(summ,ans)
+        return ans
